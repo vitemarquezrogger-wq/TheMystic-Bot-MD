a56be8e7f3341c171268bb5b083d56b0af728ec9
vitemarquezrogger-wq <vitemarquezrogger@gmail.com>
2026-04-17 17:51:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -498,16 +498,8 @@ const MethodMobile = process.argv.includes("mobile");
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 const question = (texto) => new Promise((resolver) => rl.question(texto, resolver));
 
-let opcion;
-if (methodCodeQR) opcion = '1';
-if (!methodCodeQR && !methodCode && !fs.existsSync(`./${global.authFile}/creds.json`)) {
-  do {
-    opcion = await question('[ ℹ️ ] Seleccione una opción:\n1. Con código QR\n2. Con código de texto de 8 dígitos\n---> ');
-    if (!/^[1-2]$/.test(opcion)) {
-      console.log('[ ⚠️ ] Por favor, seleccione solo 1 o 2.\n');
-    }
-  } while (opcion !== '1' && opcion !== '2' || fs.existsSync(`./${global.authFile}/creds.json`));
-}
+let opcion = '1';
+console.log('[ ✅ ] Iniciando con código QR automáticamente para Railway');
 
 const filterStrings = [
   "Q2xvc2luZyBzdGFsZSBvcGVu",
